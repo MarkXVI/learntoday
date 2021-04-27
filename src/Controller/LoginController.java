2d6356345bcf856dce945c52a566323f7afa687e
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2021-04-27 16:52:38 +0200

    Fixing with the scenes.

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 
 public class LoginController {
 
-    DB_Connection database = new DB_Connection();
+    DB_Connection database;
     @FXML
     Button registerButton;
     @FXML
--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,23 +1,34 @@
 package Model;
 
+import Controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
+
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/LoginScreen.fxml"));
         primaryStage.setTitle("Hello World");
-        Scene scene = new Scene(root, 450, 300);
+        Scene scene = new Scene(root, 900, 720);
         scene.getStylesheets().add("View/Style.css");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
     }
 
+    @Override
+    public void stop(){
+    }
 
     public static void main(String[] args) {
         launch(args);
