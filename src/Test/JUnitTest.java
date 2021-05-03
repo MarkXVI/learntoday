6e0b44f191800bf9ae1de7d3d2ce64761fbb80d1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 15:19:28 +0200

    Removing the redundant "Assert." in JUnitTest

--- src/Test/JUnitTest.java
+++ src/Test/JUnitTest.java
@@ -1,10 +1,11 @@
 package Test;
 
+import static org.junit.Assert.*;
+
 import Functionality.Logic;
 import Functionality.User;
 import Model.DB_Connection;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,27 +24,27 @@ public class JUnitTest {
     @Test
     public void check_ValidRegister() {
         Logic logic = new Logic();
-        Assert.assertFalse(logic.check_ValidRegister("", "", "", "", "Choose Account Type"));
-        Assert.assertFalse(logic.check_ValidRegister("test", "test", "test", "", "Student"));
-        Assert.assertFalse(logic.check_ValidRegister("test", "test", "", "test", "Teacher"));
-        Assert.assertFalse(logic.check_ValidRegister("test", "", "test", "test", "Student"));
-        Assert.assertFalse(logic.check_ValidRegister("", "test", "test", "test", "Student"));
-        Assert.assertFalse(logic.check_ValidRegister("test", "test", "test", "test", "Choose Account Type"));
-        Assert.assertTrue(logic.check_ValidRegister("test", "test", "test", "test", "Student"));
+        assertFalse(logic.check_ValidRegister("", "", "", "", "Choose Account Type"));
+        assertFalse(logic.check_ValidRegister("test", "test", "test", "", "Student"));
+        assertFalse(logic.check_ValidRegister("test", "test", "", "test", "Teacher"));
+        assertFalse(logic.check_ValidRegister("test", "", "test", "test", "Student"));
+        assertFalse(logic.check_ValidRegister("", "test", "test", "test", "Student"));
+        assertFalse(logic.check_ValidRegister("test", "test", "test", "test", "Choose Account Type"));
+        assertTrue(logic.check_ValidRegister("test", "test", "test", "test", "Student"));
     }
 
     @Test
     public void checkUserValues() {
         User user = new User();
-        Assert.assertTrue(user.getFirstname(), true);
-        Assert.assertTrue(user.getLastname(), true);
-        Assert.assertTrue(user.getUsername(), true);
-        Assert.assertEquals(user.getTeacher(), 0);
+        assertTrue(user.getFirstname(), true);
+        assertTrue(user.getLastname(), true);
+        assertTrue(user.getUsername(), true);
+        assertEquals(user.getTeacher(), 0);
     }
 
     @Test
     public void check_login() throws SQLException {
-        Assert.assertTrue(new DB_Connection().check_login("", ""));
+        assertTrue(new DB_Connection().check_login("", ""));
     }
 
     @After
