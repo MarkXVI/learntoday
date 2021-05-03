cb052078a3208d8dacc0d671e17e7887488e7ec0
Kristian <kristian381@hotmail.com>
2021-05-03 17:01:29 +0200

    Quiz and Read improvements

--- src/Controller/QuizController.java
+++ src/Controller/QuizController.java
@@ -6,16 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class QuizController{
+
+public class QuizController implements Initializable {
     @FXML
     Button homeButton;
+    @FXML
+    private Text topicTitle;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
     public void onHomeClick(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MainMenu.fxml"));
@@ -24,5 +31,9 @@ public class QuizController{
         scene.getStylesheets().add("View/Style.css");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void set_Title(String topic){
+        topicTitle.setText(topic);
     }
 }
--- src/Controller/ReadController.java
+++ src/Controller/ReadController.java
@@ -1,11 +1,16 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,16 +18,27 @@ public class ReadController implements Initializable {
     @FXML
     Button ttsButton;
     @FXML
+    Button homeButton;
+    @FXML
     private TextArea infoText;
     @FXML
     private Text topicText;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 
     public void set_Text(String text, String topic){
         infoText.setText(text);
         topicText.setText(topic);
+    }
+
+    public void onHomeClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MainMenu.fxml"));
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        Scene scene = new Scene(loader.load());
+        scene.getStylesheets().add("View/Style.css");
+        stage.setScene(scene);
+        stage.show();
     }
 }
