d6446af76c8b91d1b8470ceac36e385fe38e2866
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 12:45:59 +0200

    test user class

--- src/Test/JUnitTest.java
+++ src/Test/JUnitTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import Functionality.Logic;
 import Functionality.User;
+import Model.ConnectionStorage;
 import Model.DB_Connection;
 import org.junit.After;
 import org.junit.Before;
@@ -14,11 +15,9 @@ import java.sql.SQLException;
 
 public class JUnitTest {
 
-    Connection connection;
-
     @Before
     public void before() throws SQLException {
-        connection = new DB_Connection().getDBConnection();
+        Connection connection = new DB_Connection().getDBConnection();
     }
 
     @Test
@@ -49,6 +48,6 @@ public class JUnitTest {
 
     @After
     public void after() throws SQLException {
-        new DB_Connection().disconnect();
+        ConnectionStorage.getInstance().close_Connection();
     }
 }
