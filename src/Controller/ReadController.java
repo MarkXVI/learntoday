0f8628aa0100deb4f037a3755142a5cf4c5b5ef2
Simon-W1 <1simon.wikstrom@gmail.com>
2021-05-06 14:52:12 +0200

    Kan gå från read till quiz

--- src/Controller/ReadController.java
+++ src/Controller/ReadController.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import Functionality.Logic;
+import Model.QuizStorage;
+import Model.TopicStorage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ReadController implements Initializable {
@@ -19,6 +23,8 @@ public class ReadController implements Initializable {
     Button ttsButton;
     @FXML
     Button homeButton;
+    @FXML
+    Button quizButton;
     @FXML
     private TextArea infoText;
     @FXML
@@ -40,5 +46,26 @@ public class ReadController implements Initializable {
         scene.getStylesheets().add("View/Style.css");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void onQuizClick(ActionEvent actionEvent) throws IOException {
+        try {
+            String selectedTopic = TopicStorage.getInstance().getTopic();
+
+            if (Logic.checkSufficientQuestions(selectedTopic)){
+                QuizStorage.getInstance().add_questions(selectedTopic);
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/QuizLayout.fxml"));
+                Stage stage = (Stage) quizButton.getScene().getWindow();
+                Scene scene = new Scene(loader.load());
+                scene.getStylesheets().add("View/Style.css");
+                QuizController quizController = loader.getController();
+                quizController.set_Title(selectedTopic);
+
+                stage.setScene(scene);
+                stage.show();
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
