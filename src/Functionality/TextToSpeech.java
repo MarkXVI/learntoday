b34d6613cb61ff4fcd88679e4e2a2223ea81da99
XVIMark <mark990521@gmail.com>
2021-05-24 13:38:10 +0200

    TTS (NOT DONE)

--- src/Functionality/TextToSpeech.java
+++ src/Functionality/TextToSpeech.java
@@ -1,48 +1,79 @@
 package Functionality;
 
 
-import javax.speech.AudioException;
 import javax.speech.Central;
 import javax.speech.EngineException;
 import javax.speech.EngineStateError;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerModeDesc;
 import javax.speech.synthesis.Voice;
-import java.beans.PropertyVetoException;
 import java.util.Locale;
 
 
-public class TextToSpeech {
+public class TextToSpeech implements Runnable {
 
     SynthesizerModeDesc desc;
     Synthesizer synthesizer;
     javax.speech.synthesis.Voice voice;
 
-    public TextToSpeech(String text) throws EngineException, AudioException, EngineStateError, PropertyVetoException, InterruptedException {
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        desc = new SynthesizerModeDesc(Locale.US);
-        Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
-        synthesizer = Central.createSynthesizer(desc);
-        synthesizer.allocate();
-        synthesizer.resume();
-        SynthesizerModeDesc smd = (SynthesizerModeDesc) synthesizer.getEngineModeDesc();
-        Voice[] voices = smd.getVoices();
-        for (Voice voice1 : voices) {
-            if (voice1.getName().equals("kevin16")) {
-                voice = voice1;
-                break;
+    String text;
+
+    Thread Read;
+
+    public TextToSpeech() {
+        try {
+            if (desc == null) {
+                System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+                desc = new SynthesizerModeDesc(Locale.US);
+                Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+                synthesizer = Central.createSynthesizer(desc);
+                synthesizer.allocate();
+                synthesizer.resume();
+                SynthesizerModeDesc smd = (SynthesizerModeDesc) synthesizer.getEngineModeDesc();
+                javax.speech.synthesis.Voice[] voices = smd.getVoices();
+                for (Voice voice1 : voices) {
+                    if (voice1.getName().equals("kevin16")) {
+                        voice = voice1;
+                        break;
+                    }
+                }
+                synthesizer.getSynthesizerProperties().setVoice(voice);
+                Read = new Thread(this, "ReadThread");
+                Read.start();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        synthesizer.getSynthesizerProperties().setVoice(voice);
-        ReadText(text);
     }
 
     public void Terminate() throws EngineException, EngineStateError {
         synthesizer.deallocate();
     }
 
-    public void ReadText(String text) throws IllegalArgumentException, InterruptedException {
+    private synchronized void ReadText() throws IllegalArgumentException, InterruptedException {
         synthesizer.speakPlainText(text, null);
         synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+    }
+
+    @Override
+    public void run() {
+        try {
+            ReadText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void pause() throws InterruptedException {
+        Thread.currentThread().wait();
+        System.out.println("Paused");
+    }
+
+    public void stop() throws InterruptedException {
+        synthesizer.cancel();
     }
 }
